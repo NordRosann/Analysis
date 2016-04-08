1464a697ee0db69cc711fc9e7c3acb968a83f309
Oleg Dreyman <dreymonde@me.com>
2016-04-08 15:22:41 +0300

    columns computed property

--- Sources/Analysis/DataFrame.swift
+++ Sources/Analysis/DataFrame.swift
@@ -1,5 +1,15 @@
 public struct DataFrame {
     
+    public init(schema: RowSchema, rows: [List]) {
+        self.schema = schema
+        self.rows = rows.map({ $0.transformed(for: schema) })
+    }
+    
+    public init(schema: RowSchema, columns: [List]) {
+        self.schema = schema
+        self.rows = columns.lazy.difuse().map({ $0.transformed(for: schema) })
+    }
+
     public var schema: RowSchema {
         didSet {
             rows = rows.map({ $0.transformed(for: schema) })
@@ -11,21 +21,13 @@ public struct DataFrame {
             rows = rows.map({ $0.transformed(for: schema) })
         }
     }
-    
-    public init(schema: RowSchema, rows: [List]) {
-        self.schema = schema
-        self.rows = rows.map({ $0.transformed(for: schema) })
-    }
-    
-    public init(schema: RowSchema, columns: [List]) {
-        self.schema = schema
-        guard let rowsCount = columns.map({ $0.elements.count }).max() else {
-            self.rows = []
-            return
+
+    public var columns: [List] {
+        get {
+            return rows.difuse()
         }
-        self.rows = (0 ..< rowsCount).map { index in
-            let mapped = columns.map({ $0[index] })
-            return List(elements: mapped).transformed(for: schema)
+        set {
+            rows = newValue.lazy.difuse().map({ $0.transformed(for: schema) })
         }
     }
     
